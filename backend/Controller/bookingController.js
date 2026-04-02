d665c6874e681bcd97451b6bb156467c84c2f7e9
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-04-03 02:26:17 +0530

    [Reshu] Created get all the bookings and get booking by id fro Admin Purposes using findById and async await

--- backend/Controller/bookingController.js
+++ backend/Controller/bookingController.js
@@ -19,7 +19,7 @@ export const bookTicket =async (req,res)=>{
     //    check whether the tickets are available or not so that
     //  we make sure the quantity doesnot go negative
 
-    if(event.available_ticket<quantity){
+    if(event.available_tickets<quantity){
     return res.status(400).json({message:"tickets are not available,try reducing the number of tickets"});
 
     }
@@ -27,8 +27,8 @@ export const bookTicket =async (req,res)=>{
         const totalPrice=quantity*event.price;
 
          // after booking the tickets , hum ab quantity reduce kardenge
-        const remaining_tickets =event.available_ticket-quantity;
-        event.available_ticket=remaining_tickets;
+        const remaining_tickets =event.available_tickets-quantity;
+        event.available_tickets=remaining_tickets;
 
         // to save the data in our database
         await event.save();
@@ -41,5 +41,44 @@ export const bookTicket =async (req,res)=>{
     }catch(error){
         res.status(400).send(error.message);
         throw error;
+    }
+}
+
+
+
+
+
+// NOW THESE NEXT FUNCTIONS ARE FOR THE ADMIN PURPOSES
+
+
+// get all the bookings 
+
+export const getAllBooking=async(req,res)=>{
+    try{
+        const allbookings=await bookingModel.find();
+        res.status(200).json({message:"All the Bookings : ",allbookings});
+
+    }catch(error){
+        res.status(401).json({error:error.message});
+    }
+}
+
+
+
+// get all the bookings by id
+
+export const getBookingById=async(req,res)=>{
+    try{
+      const id=req.param.id;
+      const booking=await bookingModel.findById(id);
+
+      if(!booking){
+        return res.status(404).json({message:"Id not Found "});
+      }
+
+     res.status(200).json({message:"Found the booking by id ", booking});
+
+    }catch(error){
+        res.status(500).json({error:error.message});
     }
 }
