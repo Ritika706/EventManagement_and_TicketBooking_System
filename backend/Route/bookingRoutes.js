c1973f40ab8f3ecc8d2a7c1273b66355fa3122ba
Reshu Patel <reshu.patel_cs23@gla.ac.in>
2026-04-03 02:51:03 +0530

    [Reshu] Created user Schema

--- backend/Route/bookingRoutes.js
+++ backend/Route/bookingRoutes.js
@@ -1,9 +1,22 @@
 import express from 'express';
 
-import {bookTicket} from '../Controller/bookingController.js';
+import {bookTicket, getAllBooking, getBookingById} from '../Controller/bookingController.js';
 
 const router=express.Router();
 
+// to book the tickets
 router.post('/book-ticket',bookTicket);
+
+
+// FOR ADMIN
+
+
+// to get all the users
+router.get('/get-all-bookings',getAllBooking);
+
+// to get the booking by id
+router.get('/get-booking-byId/:id',getBookingById);
+
+
 
 export default router;
